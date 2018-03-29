ae11751f4b7eae37ba0e783d067d1591a1d3eca7
Shourie Ganguly <dhananjay.sah@tcs.com>
2018-03-29 20:30:30 +0530

    latest commit

--- src/services/phillipsHue.js
+++ src/services/phillipsHue.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const request = require('request');
-
+const axios = require('axios');
 module.exports.controlHue = (req, res) => {
 
     console.log(req.query);
@@ -53,24 +53,27 @@ const hueBrightness = (key, callback) => {
     let URL1 = 'http://ec2-34-208-130-135.us-west-2.compute.amazonaws.com:8080/SPConnectedHome/rest/service/setLight1Brightness?userName=Hackathon';
     let URL2 = 'http://ec2-34-208-130-135.us-west-2.compute.amazonaws.com:8080/SPConnectedHome/rest/service/setLight2Brightness?userName=Hackathon';
     
-    data = {
+   let data = {
        bri: key
     };
-    
-    request.post(URL1+key, data, (error, response, body) => {
-     
-        if(error) {
-            return callback(400, 'Honeywell service connection error');
-        } else {
-            
-            request.post(URL2+key, data, (error, response, body) => {
-     
-                if(error) {
-                    return callback(400, 'Honeywell service connection error');
-                } else {
-                    return callback(200, 'All lights brightness set');
+
+    axios.post(URL1, data, {
+        headers: {'Content-Type': 'application/json'}
+      }).then((response)=>{
+        if(response.status === 200)
+        {
+            axios.post(URL2, data, {
+                headers: {'Content-Type': 'application/json'}
+              }).then((response)=>{
+                if(response.status === 200)
+                {
+                  return callback(200, 'Command Sent');
                 }
-            });
+              }).catch((error)=> {
+                return callback(400, 'Command Failed');
+              });
         }
-    });
+      }).catch((error)=> {
+        return callback(400, 'Command Failed');
+      });
 }
